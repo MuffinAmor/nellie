c1f666844099ffee4e3b8b52f96abe208eff7350
MightyNiko <55553342+MuffinAmor@users.noreply.github.com>
2021-09-28 21:47:27 +0200

    Update apibans.py

--- lib/apibans.py
+++ lib/apibans.py
@@ -1,3 +1,25 @@
+'''MIT License
+
+Copyright (c) 2021 MightyNiko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
 import json
 import os
 
@@ -111,7 +133,7 @@ def banlist(id: int):
 
 def alertban(userid: str):
     search_term = userid
-    header = {"AuthKey": "5deeef9e4d2ca"}
+    header = {"AuthKey": ""}
     r = requests.get(
         "https://api.alertbot.services/v1/?action=bancheck&userid=%s" % (search_term), headers=header)
     if r.status_code is 200:
@@ -128,7 +150,7 @@ def alertban(userid: str):
 
 def ksoftban(userid: str):
     search_term = userid
-    header = {"Authorization": "f389c89e9f1dc2bd4f8792fd9fb79f5d00ba76ba"}
+    header = {"Authorization": ""}
     r = requests.get(
         "https://api.ksoft.si/bans/check&user=%s" % (search_term), headers=header)
     if r.status_code is 200:
